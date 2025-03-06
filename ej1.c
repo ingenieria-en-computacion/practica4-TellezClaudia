1d63c80538fb74f3fdbd5d3e27c7a5fe75d5fdd9
TellezClaudia <telleztonantzin@gmail.com>
2025-03-06 09:54:15 -0600

    Create ej1.c

---
+++ ej1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *arreglo, num, cont;
+    printf("¿Cuántos elementos tiene el conjunto?\n");
+    scanf("%d", &num);
+    arreglo = (int*)malloc(num *sizeof(int));
+    if(arreglo != NULL){ //para ver si no es vacio
+        printf("Vector reservado:\n\t[");
+        for(cont =0; cont <num; cont++){
+            printf("\t%d, %p", *(arreglo + cont), (arreglo + cont));            
+        }
+        printf("\t]\n");
+        printf("Liberando el espacio reservado\n");
+        free(arreglo);      
+        arreglo= NULL;  
+    }
+    return 0;
+}
