f6761b7f0bba52fae7c2e335a4b473e97579725d
TellezClaudia <telleztonantzin@gmail.com>
2025-03-06 09:54:52 -0600

    Create ej2.c

---
+++ ej2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *arreglo, num, cont;
+    printf("¿Cuántos elementos tiene el conjunto?\n");
+    scanf("%d", &num);
+    printf("%p\n", arreglo);
+    //calloc recibe el numero de elemnetos a reservar y el tamaño de uno de los elementos
+    arreglo = (int*)calloc(num , sizeof(int));
+    if(arreglo != NULL){
+        printf
+        ("Vector reservado:\n\t[");
+        for(cont =0; cont <num; cont++){
+            printf("\t%d %p", *(arreglo + cont), (arreglo + cont));            
+        }
+        printf("\t]\n");
+        printf("Liberando el espacio reservado\n");
+        free(arreglo);        
+    }
+    return 0;
+}
